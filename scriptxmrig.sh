2584098176ccfcf8fdc331e35e1e278f79022d7f
hhenryskate <81056140+hhenryskate@users.noreply.github.com>
2021-03-20 19:53:28 -0500

    Add files via upload

---
+++ scriptxmrig.sh
@@ -0,0 +1,13 @@
+ sudo apt-get install git build-essential cmake automake libtool autoconf -y
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build && cd xmrig/scripts
+./build_deps.sh && cd ../build
+cmake .. -DXMRIG_DEPS=scripts/deps
+make -j$(nproc)
+wget https://raw.githubusercontent.com/hhenryskate/test/main/config.json
+chmod 775 xmrig
+cp xmrig /usr/local/bin
+cp config.json /usr/local/bin
+
+
+

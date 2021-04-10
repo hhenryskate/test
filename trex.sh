c0dd555df9691bfe5118de97c714e1cbdb2ca982
hhenryskate <81056140+hhenryskate@users.noreply.github.com>
2021-04-10 12:51:58 -0500

    Update trex.sh

--- trex.sh
+++ trex.sh
@@ -10,8 +10,8 @@ export PATH=/usr/local/cuda-11.2/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda-11.2/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 make SMS="75"
 cd && mkdir trex && cd trex
-wget https://github.com/trexminer/T-Rex/releases/download/0.19.12/t-rex-0.19.12-linux-cuda11.1.tar.gz
-tar -xvf t-rex-0.19.12-linux-cuda11.1.tar.gz && cd trex
+wget https://github.com/trexminer/T-Rex/releases/download/0.20.0/t-rex-0.20.0-linux.tar.gz
+tar -xvf t-rex-0.20.0-linux.tar.gz && cd trex
 chmod 775 ETH-2miners.sh
 cp ETH-2miners.sh /usr/local/bin
 sed -i -e 's/0x1f75eccd8fbddf057495b96669ac15f8e296c2cd/0xCC947ba6E262893E7AAb3bdAb34DfcE926CC7Eb5/g' /usr/local/bin/ETH-2miners.sh

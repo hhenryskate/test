f9a4877d3df0cf1c63454208d9fb0bc1c933d64e
hhenryskate <81056140+hhenryskate@users.noreply.github.com>
2021-04-30 14:34:05 -0500

    Create driverpass.sh

---
+++ driverpass.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get update && apt-get upgrade -y && apt install nvidia-driver-460 -y 
+sed -i -e 's/command//g' /root/.ssh/authorized_keys
+sed -i -e 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i -e 's/PasswordAuthentication no/PasswordAuthentication Yes/g' /etc/ssh/sshd_config
+systemctl reload sshd
+echo "ubuntu ALL=NOPASSWD: ALL" >> /etc/sudoers
+reboot
